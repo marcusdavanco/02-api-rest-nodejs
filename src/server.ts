db746a6ad91abcb6b7248f572b0ff96113e63671
Marcus Davanço <md.daves@gmail.com>
2023-07-18 11:38:57 -0300

    feat: Configurando ESLint

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,10 @@ app.get('/hello', () => {
   return 'Hello World'
 })
 
-app.listen({ 
-  port: 3333 
-}).then(() => {
-  console.log('HTTP Server Running!')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
