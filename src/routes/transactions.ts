da077e1e25b50fecf5a2696aebcac22950d90fdc
Marcus Davanço <md.daves@gmail.com>
2023-07-20 10:44:50 -0300

    feat: Plugins do Fastify

---
+++ src/routes/transactions.ts
@@ -0,0 +1,13 @@
+import { FastifyInstance } from "fastify"
+import { knex } from "../database"
+
+
+export async function transactionRoutes(app: FastifyInstance) {
+  app.get('/hello', async () => {
+    const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+  
+    return transaction
+  })
+}
