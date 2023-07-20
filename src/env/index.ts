5f66045e94341378bb7f8f2ad57351582f3189d6
Marcus Davanço <md.daves@gmail.com>
2023-07-20 10:29:15 -0300

    feat: Requisitos da aplicação

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,8 +1,6 @@
 import 'dotenv/config'
 import { z } from 'zod'
 
-
-
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
