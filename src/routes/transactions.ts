7ef4d9fed12ab8c754087bc7eeda288db5897a13
Marcus Davanço <md.daves@gmail.com>
2023-07-21 13:43:34 -0300

    feat: Utilizando cookies no Fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -38,11 +38,23 @@ export async function transactionRoutes(app: FastifyInstance) {
 
     const { title, amount, type } = createTransactionBodySchema.parse(request.body)
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId){
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+      })
+    }
+
     await knex('transactions')
       .insert({
         id: randomUUID(),
         title,
-        amount: type === 'credit' ? amount : amount * -1
+        amount: type === 'credit' ? amount : amount * -1,
+        session_id: sessionId
 
       })
   
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+
 import { env } from './env'
 import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-// GET, POST, PUT, PATCH, DELETE
 
+app.register(cookie)
 app.register(transactionRoutes, {
   prefix: 'transactions',
 })
